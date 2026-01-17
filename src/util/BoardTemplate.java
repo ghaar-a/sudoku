c0e32183352b597010cd2c5fc46c74ec7c293725
ghaar-a <xandbox99@gmail.com>
2026-01-17 17:10:32 -0300

    jogo sudoku Java

---
+++ src/util/BoardTemplate.java
@@ -0,0 +1,23 @@
+package util;
+
+
+public final class BoardTemplate {
+
+    private BoardTemplate() {}
+
+    public static final String BOARD_TEMPLATE = """
+            *********************************************************************
+            *  %s %s %s  |  %s %s %s  |  %s %s %s  *  %s %s %s  |  %s %s %s  |  %s %s %s  *  %s %s %s  |  %s %s %s  |  %s %s %s  *
+            *  %s %s %s  |  %s %s %s  |  %s %s %s  *  %s %s %s  |  %s %s %s  |  %s %s %s  *  %s %s %s  |  %s %s %s  |  %s %s %s  *
+            *  %s %s %s  |  %s %s %s  |  %s %s %s  *  %s %s %s  |  %s %s %s  |  %s %s %s  *  %s %s %s  |  %s %s %s  |  %s %s %s  *
+            *********************************************************************
+            *  %s %s %s  |  %s %s %s  |  %s %s %s  *  %s %s %s  |  %s %s %s  |  %s %s %s  *  %s %s %s  |  %s %s %s  |  %s %s %s  *
+            *  %s %s %s  |  %s %s %s  |  %s %s %s  *  %s %s %s  |  %s %s %s  |  %s %s %s  *  %s %s %s  |  %s %s %s  |  %s %s %s  *
+            *  %s %s %s  |  %s %s %s  |  %s %s %s  *  %s %s %s  |  %s %s %s  |  %s %s %s  *  %s %s %s  |  %s %s %s  |  %s %s %s  *
+            *********************************************************************
+            *  %s %s %s  |  %s %s %s  |  %s %s %s  *  %s %s %s  |  %s %s %s  |  %s %s %s  *  %s %s %s  |  %s %s %s  |  %s %s %s  *
+            *  %s %s %s  |  %s %s %s  |  %s %s %s  *  %s %s %s  |  %s %s %s  |  %s %s %s  *  %s %s %s  |  %s %s %s  |  %s %s %s  *
+            *  %s %s %s  |  %s %s %s  |  %s %s %s  *  %s %s %s  |  %s %s %s  |  %s %s %s  *  %s %s %s  |  %s %s %s  |  %s %s %s  *
+            *********************************************************************
+            """;
+}
